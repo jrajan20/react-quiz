923127e2238b6a098d1f9f1c1b903d300e4a40ab
jrajan20 <jai_20j@hotmail.com>
2026-06-26 00:12:01 -0400

    Building React quiz app - loading questions from a server, reducer for loading questions and changing status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import {useEffect, useReducer} from "react"
 import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "././Main";
+
+const initialState={
+  questions:[],
+  status: 'loading',
+
+
+};
+
+function reducer(state,action){
+
+    switch(action.type){
+        case "dataRecieved":
+          return {...state, questions: action.payload, status: "ready"};
+        case "dataFailed":
+          return {...state, status: "error"}
+        default:
+          throw new Error("Unknown action")
+      }
+}
 
 function App() {
+  const [state, dispatch] =useReducer(reducer, initialState);
+
+  useEffect(function(){
+      fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({type: "dataRecieved", payload: data}))
+      .catch((err) => dispatch({type: 'dataFailed'}));
+  },[])
   return (
     <div className="App">
-     <DateCounter/>
+     {/* <DateCounter/> */}
+     <Header/>
+    <Main>
+      <div>
+        <p>1/15</p>
+      <p>Questions?</p>
+      </div>
+      
+    </Main>
     </div>
   );
 }
